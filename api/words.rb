d97511826b22fec4441307283faf927b84ce6d3b
David Jeusette <jeusette.david@gmail.com>
2015-10-03 22:10:37 +0200

    Extract words generation

--- api/words.rb
+++ api/words.rb
@@ -5,16 +5,22 @@ module WordsGenerator
 
       namespace :words do
         get '/' do
-          words = [
-            Forgery('basic').color,
-            Forgery('lorem_ipsum').word,
-            Forgery('name').company_name,
-            Forgery('name').female_first_name,
-            Forgery('name').industry
-          ]
+          words = Words.generate_words
 
           {words: words.sample(1 + rand(words.length)).join(' ')}
         end
+      end
+
+      private
+
+      def self.generate_words
+        [
+          Forgery('basic').color,
+          Forgery('lorem_ipsum').word,
+          Forgery('name').company_name,
+          Forgery('name').female_first_name,
+          Forgery('name').industry
+        ]
       end
     end
   end
